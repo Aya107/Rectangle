970aad0ad9e590936c28f9b58703f39f286c5b25
Aya <aya.almallah75@gmail.com>
2023-05-24 09:49:16 +0300

    main code

---
+++ Rectangle.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+import java.text.DecimalFormat;
+public class Rectangle {
+
+
+    private double length;
+    private double width;
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double calculateArea() {
+        return length * width;
+    }
+
+    public double calculatePerimeter() {
+        return 2 * (length + width);
+    }
+
+    public boolean isSquare() {
+        return length == width;
+    }
+
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle(5, 7);
+        System.out.println("Length: " + rectangle.getLength());
+        System.out.println("Width: " + rectangle.getWidth());
+        System.out.println("Area: " + rectangle.calculateArea());
+        System.out.println("Perimeter: " + rectangle.calculatePerimeter());
+        System.out.println("Is Square? " + rectangle.isSquare());
+    }
+}}
